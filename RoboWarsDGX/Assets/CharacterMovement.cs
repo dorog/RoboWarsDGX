afcc0edf4c46d862e6df3ff2d808bdd27feeeea8
dorog <dorogi.x@gmail.com>
2019-02-02 18:31:01 +0100

    Import models
    Import models

---
+++ RoboWarsDGX/Assets/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float sideSpeed = 10f;
+    [SerializeField]
+    private float rotationSpeed = 10;
+
+    private Rigidbody characterRigidBody;
+
+    void Start()
+    {
+        characterRigidBody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Vertical");
+        float side = Input.GetAxis("Horizontal");
+
+        characterRigidBody.MovePosition(transform.position + transform.forward * Time.fixedDeltaTime * speed * movement +  transform.right * Time.fixedDeltaTime * sideSpeed * side);
+
+
+        float yRot = Input.GetAxis("Mouse X");
+        Vector3 rotation = new Vector3(0, yRot, 0);
+
+        characterRigidBody.MoveRotation(characterRigidBody.rotation * Quaternion.Euler(rotation));
+    }
+}
