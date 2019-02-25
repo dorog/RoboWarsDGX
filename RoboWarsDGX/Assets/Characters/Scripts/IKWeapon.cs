70e3399e98771aab8ec4db9fd86527720b4d997d
dorog <dorogi.x@gmail.com>
2019-02-25 17:20:14 +0100

    Alex and weapon select buggfix: first version
    Alex and weapon select buggfix: first version

--- RoboWarsDGX/Assets/Characters/Scripts/IKWeapon.cs
+++ RoboWarsDGX/Assets/Characters/Scripts/IKWeapon.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using Photon.Pun;
 
-public class IKWeapon : MonoBehaviour
+public class IKWeapon : MonoBehaviourPun
 {
 
     private Animator anim;
-    private Transform GunHold;
+    public Transform GunHold;
+    public Transform rightHand;
 
     public float amount = 0.5f;
 
@@ -12,15 +14,37 @@ public class IKWeapon : MonoBehaviour
     {
         anim = GetComponent<Animator>();
 
-        Transform rightHand = anim.GetBoneTransform(HumanBodyBones.RightHand);
-        GameObject weaponGO = Instantiate(SelectData.selectedWeapon.prefab, rightHand);
-        GunHold = weaponGO.transform.GetChild(0);
+        /*Transform rightHand = anim.GetBoneTransform(HumanBodyBones.RightHand);*/
+        /*if(rightHand == null)
+        {
+            Debug.Log("null");
+        }*/
 
+        /*if (photonView.IsMine)
+        {
+            GameObject weaponGO = PhotonNetwork.Instantiate("Weapons/" + SelectData.selectedWeapon.id, Vector3.zero, Quaternion.identity, 0);
+            Vector3 position = SelectData.selectedWeapon.prefab.transform.position;
+            Quaternion rotation = SelectData.selectedWeapon.prefab.transform.rotation;
+            photonView.RPC("SetWeapon", RpcTarget.All, weaponGO.GetComponent<PhotonView>().ViewID, GetComponent<PhotonView>().ViewID, position, rotation);
+        }*/
     }
 
     private void OnAnimatorIK(int layerIndex)
     {
-        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, amount);
-        anim.SetIKPosition(AvatarIKGoal.LeftHand, GunHold.position);
+        if(GunHold != null)
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, amount);
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, GunHold.position);
+        }
     }
+
+    /*[PunRPC]
+    void SetWeapon(int weaponID, int characterID, Vector3 position, Quaternion rotation)
+    {
+        GameObject weapon = PhotonView.Find(id).gameObject;
+        weapon.transform.SetParent(rightHand);
+        weapon.transform.localPosition = position;
+        weapon.transform.localRotation = rotation;
+        GunHold = weapon.transform.GetChild(0);
+    }*/
 }
--- RoboWarsDGX/Assets/Scripts/Networking/DeathMatchManager.cs
+++ RoboWarsDGX/Assets/Scripts/Networking/DeathMatchManager.cs
@@ -5,10 +5,26 @@ public class DeathMatchManager : MonoBehaviourPun, IPunObservable
 {
 
     public GameObject[] spawnPoints;
+    //public GameObject cube;
+
+    private void Start()
+    {
+
+    }
 
     public void SpawnPlayer()
     {
-        PhotonNetwork.Instantiate("Characters/" + SelectData.selectedCharacter.id, spawnPoints[GetSpawnPoint()].transform.position, Quaternion.identity, 0);
+        GameObject character = PhotonNetwork.Instantiate("Characters/" + SelectData.selectedCharacter.id + "/" + SelectData.selectedCharacter.id + "With" + SelectData.selectedWeapon.id, spawnPoints[GetSpawnPoint()].transform.position, Quaternion.identity, 0);
+        /*int characterID = character.GetComponent<PhotonView>().ViewID;
+
+        GameObject weapon = PhotonNetwork.Instantiate("Weapons/" + SelectData.selectedWeapon.id, Vector3.zero, Quaternion.identity, 0);
+        int weaponID = weapon.GetComponent<PhotonView>().ViewID;
+
+        Vector3 position = SelectData.selectedWeapon.prefab.transform.position;
+        Quaternion rotation = SelectData.selectedWeapon.prefab.transform.rotation;
+
+        photonView.RPC("SetWeapon", RpcTarget.All, characterID, weaponID, position, rotation);*/
+        /*photonView.RPC("dfsa", RpcTarget.)*/
     }
 
     private int GetSpawnPoint()
@@ -27,4 +43,19 @@ public class DeathMatchManager : MonoBehaviourPun, IPunObservable
 
         }
     }
+
+    /*[PunRPC]
+    void SetWeapon(int characterID, int weaponID, Vector3 position, Quaternion rotation)
+    {
+        Debug.Log("Called");
+        Debug.Log(characterID + " " + weaponID);
+        GameObject character = PhotonView.Find(characterID).gameObject;
+        GameObject weapon = PhotonView.Find(weaponID).gameObject;
+
+        IKWeapon ikWeapon = character.GetComponentInChildren<IKWeapon>();
+        weapon.transform.SetParent(ikWeapon.rightHand);
+        weapon.transform.localPosition = position;
+        weapon.transform.localRotation = rotation;
+        ikWeapon.GunHold = weapon.transform.GetChild(0);
+    }*/
 }
